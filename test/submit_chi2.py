0717f45a7f1e382d633c82d5cdb2930ca2c18e05
Junghwan John Goh <jhgoh@cern.ch>
2025-07-17 11:39:44 +0900

    add argument to set number of toys

--- test/submit_chi2.py
+++ test/submit_chi2.py
@@ -5,21 +5,20 @@ import subprocess
 import numpy as np
 
 runScriptName = 'run_chi2.sbatch'
+nToys = 10000
 
-n_vals = [1000]#, 2000, 5000,
-#          10000, 20000, 50000,
-#          100000]
+n_vals = [1000, 2000, 5000, 10000]
 s_vals = np.concatenate([
-            np.logspace(-4, 0, 100),
-#           np.arange(1, 10)/10,
-#           np.arange(1, 10)/100,
-#           np.arange(1, 10)/1000,
+#            np.logspace(-4, 0, 100),
+           np.arange(1, 10)/10,
+           np.arange(1, 10)/100,
+           np.arange(1, 10)/1000,
          ])
 m_vals = np.concatenate([
-            np.logspace(-2, 1, 100),
-#           np.arange(1, 10)/100,
-#           np.arange(1, 10)/10,
-#           np.arange(1, 10)/1,
+#            np.logspace(-2, 1, 100),
+           np.arange(1, 10)/100,
+           np.arange(1, 10)/10,
+           np.arange(1, 10)/1,
          ])
 
 if not os.path.exists('results'):
@@ -28,12 +27,11 @@ if not os.path.exists('results'):
 s_str = ','.join([f'{x:g}' for x in s_vals])
 s_str2 = s_str.replace(',', '_')
 for i, (m, n) in enumerate(itertools.product(m_vals, n_vals)):
-  #suffix = f'sin14_{s_str2}__dm41_{m}__nSignal_{n}'.replace('.', 'p')
   suffix = f'iter_{i}__dm41_{m}__nSignal_{n}'.replace('.', 'p')
   jobName = f'Chi2_{suffix}'
   fName = f'results/result__{suffix}.root'
   cmd = ['sbatch', f'--job-name={jobName}',
-         f'--export=V_SIN14="{s_str}",V_DM41={m},V_NSIG={n},V_FNAME={fName}',
+         f'--export=V_SIN14="{s_str}",V_DM41={m},V_NSIG={n},V_FNAME={fName},V_NTOYS={nToys}',
          runScriptName]
 
   if os.path.exists(fName):
