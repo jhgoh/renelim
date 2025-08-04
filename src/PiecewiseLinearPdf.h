820cf566e1e270e9b4c7700272c9de3ac499b144
Junghwan John Goh <jhgoh@cern.ch>
2025-08-04 14:36:00 +0900

    Avoid indented includes

--- src/PiecewiseLinearPdf.h
+++ src/PiecewiseLinearPdf.h
@@ -1,32 +1,34 @@
 #ifndef PiecewiseLinearPdf_H
 #define PiecewiseLinearPdf_H
 
-#include "RooAbsPdf.h"
-#include "RooRealProxy.h"
-#include "RooCategoryProxy.h"
-#include "RooAbsReal.h"
 #include "RooAbsCategory.h"
+#include "RooAbsPdf.h"
+#include "RooAbsReal.h"
+#include "RooCategoryProxy.h"
+#include "RooRealProxy.h"
 
 #include <vector>
- 
+
 class PiecewiseLinearPdf : public RooAbsPdf {
 public:
   PiecewiseLinearPdf() = default;
-  PiecewiseLinearPdf(const char *name, const char *title, RooAbsReal& x,
-                     const int n, const double* xx, const double* yy);
-  PiecewiseLinearPdf(const PiecewiseLinearPdf& other, const char* name=0);
-  virtual TObject* clone(const char* newname) const { return new PiecewiseLinearPdf(*this,newname); }
-  inline virtual ~PiecewiseLinearPdf() = default; 
+  PiecewiseLinearPdf(const char *name, const char *title, RooAbsReal &x, const int n,
+                     const double *xx, const double *yy);
+  PiecewiseLinearPdf(const PiecewiseLinearPdf &other, const char *name = 0);
+  virtual TObject *clone(const char *newname) const {
+    return new PiecewiseLinearPdf(*this, newname);
+  }
+  inline virtual ~PiecewiseLinearPdf() = default;
 
 protected:
   RooRealProxy x_;
   std::vector<double> xx_, yy_;
-  
-  void set(const int n, const double* xx, const double* yy);
+
+  void set(const int n, const double *xx, const double *yy);
   double evaluate() const;
 
 private:
-  ClassDef(PiecewiseLinearPdf,1) // Your description goes here...
+  ClassDef(PiecewiseLinearPdf, 1) // Your description goes here...
 };
- 
+
 #endif
